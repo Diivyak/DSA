799b7e9a38cfcffccb1c907421b6c9da736d33b6
Diivyak <divyani.02@gmail.com>
2022-04-16 22:50:34 +0530

    16-Apr-2022

--- src/com/arrays/medium/SpiralTraversal.java
+++ src/com/arrays/medium/SpiralTraversal.java
@@ -37,6 +37,7 @@ public class SpiralTraversal {
 	public static void main(String[] args) {
 //		int[][] A ={{4, 2, 3, 6, 7, 8, 1, 9, 5, 10},
 //					{12, 19, 15, 16, 20, 18, 13, 17, 11, 14}};
+		
 		int[][] A = {{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}};
 //		int[][] A = {{1, 2, 3, 4},
 //			    {12, 13, 14, 5},
