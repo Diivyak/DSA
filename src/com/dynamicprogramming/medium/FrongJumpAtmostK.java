687af0c686d9d8fac26d840094978f5f78d1cc73
Diivyak <divyani.02@gmail.com>
2022-04-17 23:40:30 +0530

    17-Apr-2022

--- src/com/dynamicprogramming/medium/FrongJumpAtmostK.java
+++ src/com/dynamicprogramming/medium/FrongJumpAtmostK.java
@@ -1,9 +1,25 @@
 package com.dynamicprogramming.medium;
 
+import java.util.Arrays;
+
 public class FrongJumpAtmostK {
 
+	public static int frogJumpAtmostK(int[] heights, int k) {
+		int n = heights.length;
+		int[] dp = new int[n];
+		Arrays.fill(dp, (int)1e8);
+		dp[0] = 0;
+		for(int i=1; i<n;i++) {
+			for(int j= k; j>=1; j--) {
+				if(i-j >= 0)
+					dp[i] = Math.min(dp[i], dp[i-j] + Math.abs(heights[i-j] - heights[i]));
+			}
+		}
+		return dp[n-1];
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[]  heights = {40, 10 ,20, 70, 80, 10, 20, 70, 80, 60};
+		System.out.println(frogJumpAtmostK(heights,4));
 
 	}
 
