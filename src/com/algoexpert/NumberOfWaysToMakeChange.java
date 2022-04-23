2bc5a2a189c9fd85bbed7d96c31d03672d45af10
Diivyak <divyani.02@gmail.com>
2022-04-23 13:16:29 +0530

    23-Apr-2022

---
+++ src/com/algoexpert/NumberOfWaysToMakeChange.java
@@ -0,0 +1,25 @@
+package com.algoexpert;
+
+public class NumberOfWaysToMakeChange {
+
+	public static int numberOfWaysToMakeChange(int n, int[] denoms) {
+	    int len = denoms.length;
+			int[][] dp = new int[len+1][n+1];
+			dp[0][0] = 1;
+			for(int i=1; i<=len;i++) {
+				for(int j=0; j<=n;j++) {
+				
+					dp[i][j] = dp[i-1][j] + (j >= denoms[i-1] ? dp[i][j-denoms[i-1]] : 0);
+				}
+			}
+	    return dp[len][n];
+	  }
+	public static void main(String[] args) {
+		int n = 10;
+		int[] A = {1,5,10,25};
+		
+		System.out.println(numberOfWaysToMakeChange(n, A));
+
+	}
+
+}
