805dc16f61b8ead24dce1589bee80a6c6652b0e7
Diivyak <divyani.02@gmail.com>
2022-05-03 20:44:08 +0530

    03-May-2022

--- src/com/neetcode/CarFleet.java
+++ src/com/neetcode/CarFleet.java
@@ -1,37 +1,40 @@
 package com.neetcode;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Stack;
 
 public class CarFleet {
 
-public static int carFleet(int target, int[] position, int[] speed) {
-        
-        int n = position.length;
-        Double[][] targetList = new Double[n][2];
-//        for(int i=0; i<n;i++)
-//            targetList[i] = (1.0*(target - position[i]))/speed[i];
-        Arrays.sort(targetList, (a, b)-> b[0] - a[0]);
-//        Arrays.sort(targetList, (double[] a, double[] b)-> b[0] - a[0]);
-//        Arrays.sort(targetList, (double[] a, int[] b)->{
-//        Arrays.sort(targetList, new sort(double a, double n));
-//        Arrays.sort(targetList, (double[] a, double[] b)->{
-//            if(a[0] == b[0]) return a[1] - b[1];
-//            return b[0] - a[0];
-//        });
-        Stack<Double> stack = new Stack<Double>();
-        for(int i=0; i<n;i++) {
-            if(stack.isEmpty() || (!stack.isEmpty() && stack.peek() < targetList[i])) 
-                stack.add(targetList[i]);	
-            
-        }
-        return stack.size();		
+	public static int carFleet(int target, int[] position, int[] speed) {
+		int n = position.length;
+		ArrayList<int[]> list = new ArrayList<int[]>();
+		
+		for(int i=0; i<n;i++) {
+			list.add(new int[] {position[i], speed[i]});
+		}
+		Collections.sort(list, (int[] a, int[] b)-> b[0] - a[0]);
+		
+        double sLowestTime = 0;
+        int count =0;
+		for(int i=0; i<n;i++) {
+			double time = 1.0*(target- list.get(i)[0])/list.get(i)[1];
+			if(time > sLowestTime) {
+                count++;
+            }
+            sLowestTime = Math.max(sLowestTime, time);
+		}
+		return count;
     }
 	public static void main(String[] args) {
-		int[] position = {10,8,0,5,3};
-		int[] speed = {2,4,1,1,3};
-		System.out.println(carFleet(12, position, speed));
+//		int[] position = {10,8,0,5,3};
+//		int[] speed = {2,4,1,1,3};
+//		12
+		int[] position = {6,8};
+		int[] speed = {3,2};
+//		10
+//		[6,8]
+//		[3,2]
+		System.out.println(carFleet(10, position, speed));
 
 	}
 
