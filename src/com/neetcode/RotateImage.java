d8dac5315493f089ee7980a2caf2ba3e8bde61cf
Diivyak <divyani.02@gmail.com>
2022-05-07 23:54:31 +0530

    07-May-2022

--- src/com/neetcode/RotateImage.java
+++ src/com/neetcode/RotateImage.java
@@ -2,9 +2,29 @@ package com.neetcode;
 
 public class RotateImage {
 
+	public static void rotate(int[][] matrix) {
+        int size = matrix.length;
+        
+        for(int i=0; i<size;i++) {
+            for(int j=i; j<size;j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+        for(int i=0; i<size;i++) {
+            for(int j=0; j<size/2;j++) {
+                int tmp = matrix[i][size-1-j];
+                matrix[i][size-1-j] = matrix[i][j];
+                matrix[i][j] = tmp;
+            }
+        }
+        return;
+        
+    }
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[][] arr = {{1,2,3},{4,5,6},{7,8,9}};
+		rotate(arr);
 	}
 
 }
